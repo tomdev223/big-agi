cfdaafe75d731d0a9fa849a214f5ee04a94e4a7e
Full-stack developer <lovetom.pro@gmail.com>
2024-01-04 22:02:56 +0200

    front cors

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -2,5 +2,5 @@
 // export const { NEXT_PUBLIC_SERVER_PORT, NEXT_PUBLIC_CLIENT_PORT, NEXT_PUBLIC_SERVER_HOST } = process.env;
 export const NEXT_PUBLIC_SERVER_PORT = '3001';
 export const NEXT_PUBLIC_CLIENT_PORT = '3000';
-// export const NEXT_PUBLIC_SERVER_HOST = '127.0.0.1';
-export const NEXT_PUBLIC_SERVER_HOST = '3.13.141.173';
+export const NEXT_PUBLIC_SERVER_HOST = '127.0.0.1';
+// export const NEXT_PUBLIC_SERVER_HOST = '3.13.141.173';
