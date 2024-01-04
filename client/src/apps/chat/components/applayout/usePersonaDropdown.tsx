9df85380eb4ad8af0aae388f8f0e19c8dbe87186
Full-stack developer <lovetom.pro@gmail.com>
2024-01-04 21:45:14 +0200

    1

--- client/src/apps/chat/components/applayout/usePersonaDropdown.tsx
+++ client/src/apps/chat/components/applayout/usePersonaDropdown.tsx
@@ -107,7 +107,6 @@ function AppBarPersonaDropdown(props: {
         const config: any = {
           headers: {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
           },
         };
         const response = await axios.get(url, config);
