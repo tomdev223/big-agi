156e0f93cad0338c910fccc96ea7f31561e1a811
tsguru721 <lovetom.pro@gmail.com>
2024-01-19 16:46:32 +0200

    url fix

--- client/src/modules/elevenlabs/elevenlabs.client.ts
+++ client/src/modules/elevenlabs/elevenlabs.client.ts
@@ -77,8 +77,7 @@ export async function EXPERIMENTAL_speakTextStream(text: string, voiceId?: strin
 
   // const edgeResponse = await fetchApiElevenlabsSpeech(text, elevenLabsApiKey, voiceId || elevenLabsVoiceId, nonEnglish, true);
    //Piper for TTS
-    const edgeResponse: any = await axios.get(`https://aitools.lamassucrm.com/piper
-    /tts?model=US-danny&pitch=1`, 
+    const edgeResponse: any = await axios.get(`https://aitools.lamassucrm.com/piper/tts?model=US-danny&pitch=1`, 
     {
       headers: {
       'Access-Control-Allow-Origin': '*',
