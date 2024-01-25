3285236ffc56983f4cd276c84a5f2c98443b0927
Full-stack developer <lovetom.pro@gmail.com>
2024-01-25 08:40:10 -0500

    mpeg

--- client/src/common/util/AudioLivePlayer.ts
+++ client/src/common/util/AudioLivePlayer.ts
@@ -33,7 +33,7 @@ export class AudioLivePlayer {
     const sourceNode = this.audioContext.createMediaElementSource(this.audioElement);
     sourceNode.connect(this.audioContext.destination);
 
-    const mimeType = 'audio/wav';
+    const mimeType = 'audio/mpeg';
     this.mediaSource.addEventListener('sourceopen', async () => {
       const sourceBuffer: SourceBuffer = this.mediaSource.addSourceBuffer(mimeType);
       this.reader = esgeReadableStream.getReader();
