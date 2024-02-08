7a97776ed1dd8b2cca436d978d064054c5f1f15f
Full-stack developer <lovetom.pro@gmail.com>
2024-02-08 07:43:09 -0500

    lamtts endpoint for tts

--- client/src/modules/elevenlabs/elevenlabs.client.ts
+++ client/src/modules/elevenlabs/elevenlabs.client.ts
@@ -77,7 +77,7 @@ async function fetchApiElevenlabsSpeech(text: string, elevenLabsApiKey: string, 
     ...(streaming && { streaming: true, streamOptimization: 4 }),
   };
 
-  const response = await fetch('https://aitools.lamassucrm.com/piper/tts?'+'language='+personaLanguage+'&model='+personaModelName+'&pitch=1', {
+  const response = await fetch('https://aitools.lamassucrm.com/lamtts/tts?'+'language='+personaLanguage+'&model='+personaModelName+'&pitch=1', {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
