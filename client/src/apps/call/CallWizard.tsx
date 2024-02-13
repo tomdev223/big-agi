5c4ced684d265e39f0dfcf67004f821a58c8c673
Full-stack developer <lovetom.pro@gmail.com>
2024-02-13 16:28:43 -0500

    ignore empty chat

--- client/src/apps/call/CallWizard.tsx
+++ client/src/apps/call/CallWizard.tsx
@@ -77,7 +77,7 @@ function StatusCard(props: { icon: React.JSX.Element, hasIssue: boolean, text: s
 
 export function CallWizard(props: { strict?: boolean, conversationId: string, children: React.ReactNode }) {
   // state
-  const [chatEmptyOverride, setChatEmptyOverride] = React.useState(false);
+  const [chatEmptyOverride, setChatEmptyOverride] = React.useState(true);
   const [recognitionOverride, setRecognitionOverride] = React.useState(false);
 
   // external state
